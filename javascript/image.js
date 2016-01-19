903e7f1ba142c4c77fb3bbcdd92fa03495b47c79
Kyle Pfromer <kpfromer2@gmail.com>
2016-01-19 12:45:48 -0700

    Revert "Improved Animations, But slow"
    This reverts commit c08fb683564b4d4b749e7e2df25edd608268c2c4.

--- javascript/image.js
+++ javascript/image.js
@@ -1,19 +1,19 @@
-$(window).load(function() {
+$(document).ready(function(){
   setTimeout(function() {
-    $("#loader").fadeOut(200);
+    $("#loader").fadeOut(250);
     setTimeout(function() {
       $("#loader").remove();
       $("#intro").css("animation-play-state","running");
       $("#intro").css("-webkit-animation-play-state","running");
-      //setTimeout(function () {
-        //$("#title").css("animation-play-state","running");
-        //$("#title").css("-webkit-animation-play-state","running");
+      setTimeout(function () {
+        $("#title").css("animation-play-state","running");
+        $("#title").css("-webkit-animation-play-state","running");
         setTimeout(function () {
-          $("#intro").remove();
           $("#mainmenu").css("animation-play-state","running");
           $("#mainmenu").css("-webkit-animation-play-state","running");
-        //}, 1500);
+        }, 1500);
       }, 1500);
     },350);
   }, 1500);
+
 });
--- javascript/randomcolor.js
+++ javascript/randomcolor.js
@@ -1,1 +1,1 @@
-var colors = [["#2f4a7c","#bab73a","#b53942"]];
+dasdas
